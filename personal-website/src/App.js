0e6b048dc9d6e38c7fb45cd9fd8902eca72f78f8
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-23 12:29:50 +0100

    main content for about me section added

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import './App.css'
+import AboutMe from './components/AboutMe'
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<HeroSection />
+			<AboutMe />
 		</>
 		// <BrowserRouter>
 		// 	<NavBar />
---
+++ personal-website/src/components/AboutMe.jsx
@@ -0,0 +1,34 @@
+export default function AboutMe() {
+	return (
+		<>
+			<section className="about-me-container">
+				<div className="about-me-text">
+					<div>
+						Hey! I'm Chris and I want to build stuff for the internet. I am an
+						aspiring front end developer with a background in hospitality. I
+						enjoyes problem solving, teaching and developing leadership skills.
+					</div>
+
+					<div>
+						I find my passion for tech in the front end, specifically in UI
+						design and implementation. I have always tended toward more creative
+						outlets that have a degree of skill and find coding to be the
+						perfect mix for me. I love seeing how a page slowly comes together
+						and tweaking those small details to make it extra special.
+					</div>
+				</div>
+				<div className="tech-list">
+					Some of the tech I have worked with...
+					<div className="tech-list-items">
+						<ul>
+							<li>React</li>
+							<li>Tailwind</li>
+							<li>MUI</li>
+							<li>Next.js</li>
+						</ul>
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
