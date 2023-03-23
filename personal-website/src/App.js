7979b0af38181d2be22e824f3745b4e681a040b2
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-23 11:38:15 +0100

    hero section finished

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import './App.css'
 
 function App() {
 	return (
-		<BrowserRouter>
+		<>
 			<NavBar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+			<HeroSection />
+		</>
+		// <BrowserRouter>
+		// 	<NavBar />
+		// 	<Routes>
+		// 		<Route path="/" element={<Home />} />
+		// 	</Routes>
+		// </BrowserRouter>
 	)
 }
 
--- personal-website/src/components/HeroSection.jsx
+++ personal-website/src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HeroSection() {
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
@@ -14,29 +14,28 @@ export default function Home() {
 
 	return (
 		<>
-			<section className="intro-content">
+			<section className="intro-content-container">
 				<div className="hidden">
-					<h1 className="main-text">Chris Wagg - Aspiting web developer</h1>
-					<h2 className="secondary-text">
-						<div>
-							Thanks for checking out my website. It's not much, but it's honest
+					<div className="intro-text">
+						<div className="secondary-text-green">Hi, I'm...</div>
+						<h1 className="main-text">Chris Wagg - Aspiring web developer</h1>
+						<div className="secondary-text">
+							Thanks for checking out my website! It's not much, but it's honest
 							work.
 						</div>
-						<div>
+						<div className="secondary-text">
 							I am a front end web developer with a background in hospitality
 							who enjoyes problem solving, teaching and developing leadership
 							skills.
 						</div>
-						<div>
+						<div className="secondary-text">
 							I find my passion for tech in the front end, specifically in UI
-							design and implementation. I have always tended toward more
-							creative outlets that have a degree of skill and find coding to be
-							the perfect mix for me. I love seeing how a page slowly comes
-							together and tweaking those small details to make it extra
-							special.
+							design and implementation.
 						</div>
-						<div>Check out some of my stuff down below!</div>
-					</h2>
+						<div className="secondary-text">
+							Check out some of my stuff down below!
+						</div>
+					</div>
 				</div>
 			</section>
 		</>
