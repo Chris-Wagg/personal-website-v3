a507b751c97ba917e5bd7fced23609497be202b3
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-23 12:35:52 +0100

    projects block added

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -4,6 +4,7 @@ import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import './App.css'
 import AboutMe from './components/AboutMe'
+import Projects from './components/Projects'
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<NavBar />
 			<HeroSection />
 			<AboutMe />
+			<Projects />
 		</>
 		// <BrowserRouter>
 		// 	<NavBar />
