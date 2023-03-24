aa600c8eac12b92249d1bd8ab47e822f49fde5e0
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-24 13:30:37 +0100

    odin project projects added to projects list

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -1,7 +1,5 @@
 import ProjectCard from './ProjectCard'
 
-// const { projectName, aboutProject, projectTech, projectLink } = props
-
 export default function Projects() {
 	return (
 		<div className="projects-container">
@@ -13,6 +11,24 @@ export default function Projects() {
                     and development"
 					projectTech="React, MUI"
 					projectLink="https://github.com/manaia-2021/KIND"
+				/>
+				<ProjectCard
+					projectName="TOP Sign up form"
+					aboutProject="A project using flex-box with a little bit of grid. This project focused on forms and input validation. The other branch of this project includes the Constraint Validation API to validate all the inputs as the user interacts with them."
+					projectTech="HTML, CSS"
+					projectLink="https://github.com/Chris-Wagg/TOP-sign-up-form"
+				/>
+				<ProjectCard
+					projectName="TOP Admin Dashboard"
+					aboutProject="A challenge from The Odin Project using both flex and grid concepts together to create a flexible and dynamic page with multiple elements"
+					projectTech="HTML, CSS"
+					projectLink="https://github.com/Chris-Wagg/TOP-Admin-Dashboard"
+				/>
+				<ProjectCard
+					projectName="Top Landing page"
+					aboutProject="Another Odin Project challenge to put multiple flexbox concepts together to make one webpage."
+					projectTech="HTML, CSS"
+					projectLink="https://github.com/Chris-Wagg/TOP-landing-page"
 				/>
 			</section>
 		</div>
