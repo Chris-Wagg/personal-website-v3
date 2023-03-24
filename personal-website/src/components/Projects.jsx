6481b17ca201f8115024bd5b2498c5dc4e2bdfec
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-24 12:57:01 +0100

    projects page refactored for props

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -1,27 +1,29 @@
+import ProjectCard from './ProjectCard'
+
+// const { projectName, aboutProject, projectTech, projectLink } = props
+
 export default function Projects() {
 	return (
 		<div className="projects-container">
 			<section className="projects">
-				<div className="project-card-container">
-					<div className="icon"></div>
-					<div className="project-name">KIND</div>
-					<div className="about-project">
-						The final project of my coding bootcamp that was built with 4 others
-						in a little under a week. I was responsible for all the UI design
-						and development
-					</div>
-					<div className="project-tech-list"> - React, MUI</div>
-					<button className="project-link">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://github.com/manaia-2021/KIND"
-						>
-							Check it out!
-						</a>
-					</button>
-				</div>
+				<ProjectCard
+					projectName="KIND"
+					aboutProject="The final project of my coding bootcamp that was built with 4 others
+                    in a little under a week. I was responsible for all the UI design
+                    and development"
+					projectTech="React, MUI"
+					projectLink="https://github.com/manaia-2021/KIND"
+				/>
 			</section>
 		</div>
 	)
+}
+
+{
+	/* <ProjectCard
+					projectName=""
+					aboutProject=""
+					projectTech=""
+					projectLink=""
+				/> */
 }
