50626047ec533d7ef07f3d5757a8283dc1fc178b
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-24 11:55:53 +0100

    about my info all added

--- personal-website/src/components/AboutMe.jsx
+++ personal-website/src/components/AboutMe.jsx
@@ -1,31 +1,47 @@
 export default function AboutMe() {
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('show')
+			} else {
+				entry.target.classList.remove('show')
+			}
+		})
+	})
+
+	const hiddenElements = document.querySelectorAll('.hidden')
+	hiddenElements.forEach((el) => observer.observe(el))
+
 	return (
 		<>
 			<section className="about-me-container">
 				<div className="hidden">
 					<div className="about-me-text">
+						<h1>About me...</h1>
 						<div>
-							Hey! I'm Chris and I want to build stuff for the internet. I am an
-							aspiring front end developer with a background in hospitality. I
-							enjoyes problem solving, teaching and developing leadership
-							skills.
+							Hey! I'm Chris and I want to build stuff for the internet. My
+							journey to become a developer started in 2021 when I started
+							looking into deveopment for something new and fun to do.
 						</div>
 
 						<div>
-							I find my passion for tech in the front end, specifically in UI
-							design and implementation. I have always tended toward more
-							creative outlets that have a degree of skill and find coding to be
-							the perfect mix for me. I love seeing how a page slowly comes
-							together and tweaking those small details to make it extra
-							special.
+							Various online courses, a bootcamp, moving to the other side of
+							the world and working a full time job later, here I am trying to
+							chase my dream.
 						</div>
+						<div>
+							I have learnt a whole lot about the big wide world of coding over
+							the last few years and am excited to bring that to a job and
+							continue to learn and show what I can do!
+						</div>
+						<div>Check out some of my projects below!</div>
 					</div>
 					<div className="tech-list">
 						Some of the tech I have worked with...
 						<div className="tech-list-items">
 							<ul>
 								<li>React</li>
-								<li>Tailwind</li>
+								{/* <li>Tailwind</li> */}
 								<li>MUI</li>
 								<li>Next.js</li>
 							</ul>
