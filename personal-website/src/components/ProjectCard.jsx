57313331a82a7efa156038d587cadde9f1ddb893
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-24 12:52:27 +0100

    project card component added

---
+++ personal-website/src/components/ProjectCard.jsx
@@ -0,0 +1,23 @@
+export default function ProjectCard(props) {
+	return (
+		<div className="project-card-container">
+			<div className="icon"></div>
+			<div className="project-name">KIND</div>
+			<div className="about-project">
+				The final project of my coding bootcamp that was built with 4 others in
+				a little under a week. I was responsible for all the UI design and
+				development
+			</div>
+			<div className="project-tech-list"> - React, MUI</div>
+			<button className="project-link">
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/manaia-2021/KIND"
+				>
+					Check it out!
+				</a>
+			</button>
+		</div>
+	)
+}
