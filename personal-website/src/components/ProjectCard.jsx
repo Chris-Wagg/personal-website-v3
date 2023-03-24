501e9662bad8fd13dbb474947bd742f6a376a804
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-24 12:52:59 +0100

    project card compoent refactored for props

--- personal-website/src/components/ProjectCard.jsx
+++ personal-website/src/components/ProjectCard.jsx
@@ -1,20 +1,13 @@
 export default function ProjectCard(props) {
+	const { projectName, aboutProject, projectTech, projectLink } = props
 	return (
 		<div className="project-card-container">
 			<div className="icon"></div>
-			<div className="project-name">KIND</div>
-			<div className="about-project">
-				The final project of my coding bootcamp that was built with 4 others in
-				a little under a week. I was responsible for all the UI design and
-				development
-			</div>
-			<div className="project-tech-list"> - React, MUI</div>
+			<div className="project-name">{projectName}</div>
+			<div className="about-project">{aboutProject}</div>
+			<div className="project-tech-list"> - {projectTech}</div>
 			<button className="project-link">
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					href="https://github.com/manaia-2021/KIND"
-				>
+				<a target="_blank" rel="noopener noreferrer" href={projectLink}>
 					Check it out!
 				</a>
 			</button>
