12ef68b1ef191fad42c3ed4b89c004b3b45811a1
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-28 19:41:43 +0200

    added reduced motion media query

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -8,13 +8,25 @@ import Projects from './pages/Projects'
 import Experience from './pages/Experience'
 
 function App() {
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('show')
+			} else {
+				entry.target.classList.remove('show')
+			}
+		})
+	})
+
+	const hiddenElements = document.querySelectorAll('.hidden')
+	hiddenElements.forEach((el) => observer.observe(el))
 	return (
 		<>
 			<NavBar />
-			<HeroSection />
-			<AboutMe />
-			<Projects />
-			<Experience />
+			<HeroSection className="hidden" />
+			<AboutMe className="hidden" />
+			<Projects className="hidden" />
+			<Experience className="hidden" />
 		</>
 	)
 }
