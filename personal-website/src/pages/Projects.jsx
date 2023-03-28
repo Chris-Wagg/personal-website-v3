80c350a7116c9c170d3eba6c188fabdc0f1db324
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-28 15:04:28 +0200

    projects cards links fixed to have both github and live links

--- personal-website/src/pages/Projects.jsx
+++ personal-website/src/pages/Projects.jsx
@@ -24,36 +24,31 @@ export default function Projects() {
 									in a little under a week. I was responsible for all the UI design
 									and development"
 					projectTech="React, MUI"
-					projectLink="https://github.com/manaia-2021/KIND"
+					liveLink="https://kind-manaia.herokuapp.com"
+					githubLink="https://github.com/manaia-2021/KIND"
 				/>
 				<ProjectCard
 					projectName="TOP Sign up form"
 					aboutProject="A project using flex-box with a little bit of grid. This project focused on forms and input validation. The other branch of this project includes the Constraint Validation API to validate all the inputs as the user interacts with them."
 					projectTech="HTML, CSS"
-					projectLink="https://github.com/Chris-Wagg/TOP-sign-up-form"
+					liveLink="https://chris-wagg.github.io/TOP-sign-up-form/"
+					githubLink="https://github.com/Chris-Wagg/TOP-sign-up-form"
 				/>
 				<ProjectCard
 					projectName="TOP Admin Dashboard"
 					aboutProject="A challenge from The Odin Project using both flex and grid concepts together to create a flexible and dynamic page with multiple elements"
 					projectTech="HTML, CSS"
-					projectLink="https://github.com/Chris-Wagg/TOP-Admin-Dashboard"
+					liveLink="https://chris-wagg.github.io/TOP-Admin-Dashboard/"
+					githubLink="https://github.com/Chris-Wagg/TOP-Admin-Dashboard"
 				/>
 				<ProjectCard
 					projectName="Top Landing page"
 					aboutProject="Another Odin Project challenge to put multiple flexbox concepts together to make one webpage."
 					projectTech="HTML, CSS"
-					projectLink="https://github.com/Chris-Wagg/TOP-landing-page"
+					liveLink="https://chris-wagg.github.io/TOP-landing-page/"
+					githubLink="https://github.com/Chris-Wagg/TOP-landing-page"
 				/>
 			</div>
 		</section>
 	)
-}
-
-{
-	/* <ProjectCard
-					projectName=""
-					aboutProject=""
-					projectTech=""
-					projectLink=""
-				/> */
 }
