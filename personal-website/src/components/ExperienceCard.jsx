e96197d10943d136596ec947e862025620c1528a
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-03-28 17:01:37 +0200

    experience section styling finished

--- personal-website/src/components/ExperienceCard.jsx
+++ personal-website/src/components/ExperienceCard.jsx
@@ -1,6 +1,11 @@
-export default function EXperienceCard(props) {
-	const { experienceName, experienceDate, aboutExperience, experienceTech } =
-		props
+export default function ExperienceCard(props) {
+	const { experienceName, experienceDate, aboutExperience } = props
+
+	// const button = document.getElementById('summary-button')
+	// const text = document.getElementById('summary-text')
+	// const textFade = () => console.log(text)
+	// button.addEventListener('onClick', textFade)
+
 	return (
 		<div className="experience-card-container">
 			<p className="card-title">{experienceName}</p>
