3ab7acd723d4e5008f96b972b5bcb6ae8953f8b1
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-22 14:27:18 +0100

    travel page project added. Tech stack workd with updated

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,20 +1,23 @@
 export default function AboutMe() {
 	return (
 		<>
-			<section className="secondary-container " id="scroll-target-about">
-				<h1 className="title-text">About me</h1>
-				<div className="text-container">
+			<section className='secondary-container ' id='scroll-target-about'>
+				<h1 className='title-text'>About me</h1>
+				<div className='text-container'>
 					<p>
-						Hey! I'm Chris and I want to build stuff for the internet. My
-						journey to become a developer started in 2021 when I started looking
-						into deveopment for something new and fun to do. Various online
-						courses, a bootcamp, moving to the other side of the world and
-						working a full time job later, here I am trying to chase my dream.
+						Hey! I'm Chris and I want to build stuff for the
+						internet. My journey to become a developer started in
+						2021 when I started looking into deveopment for
+						something new and fun to do. Various online courses, a
+						bootcamp, moving to the other side of the world and
+						working a full time job later, here I am trying to chase
+						my dream.
 					</p>
 					<p>
-						I have learnt a whole lot about the big wide world of coding over
-						the last few years and am excited to bring that to a job and
-						continue to learn and show what I can do!
+						I have learnt a whole lot about the big wide world of
+						coding over the last few years and am excited to bring
+						that to a job and continue to learn and show what I can
+						do!
 					</p>
 					<p>
 						Some of the tech I have worked with...
@@ -22,7 +25,8 @@ export default function AboutMe() {
 							<li>React</li>
 							<li>MUI</li>
 							<li>Next.js</li>
-							{/* <li>Prisma</li> */}
+							<li>Prisma</li>
+							<li>Tailwind</li>
 						</ul>
 					</p>
 				</div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,38 +2,45 @@ import ProjectCard from '../components/ProjectCard'
 
 export default function Projects() {
 	return (
-		<section className="secondary-container" id="scroll-target-projects">
-			<h1 className="title-text">Projects</h1>
-			<div className="project-cards-container">
+		<section className='secondary-container' id='scroll-target-projects'>
+			<h1 className='title-text'>Projects</h1>
+			<div className='project-cards-container'>
 				<ProjectCard
-					projectName="KIND"
-					aboutProject="The final project of my coding bootcamp that was built with 4 others
+					projectName='M.E. Travel'
+					aboutProject='A self directed project to copy a full single page app with a focus on accessability and responsivity. Originally built with Typescrit and Tailwind, I reverted to CSS3 feeling it reduced bloat in my html and was generally easier for me to work with.'
+					projectTech='Next.js, CSS'
+					liveLink='https://chris-wagg.github.io/travel-v2/'
+					githubLink='https://github.com/Chris-Wagg/travel-v2'
+				/>
+				<ProjectCard
+					projectName='KIND'
+					aboutProject='The final project of my coding bootcamp that was built with 4 others
 									in a little under a week. I was responsible for all the UI design
-									and development"
-					projectTech="React, MUI"
-					liveLink="https://kind-manaia.herokuapp.com"
-					githubLink="https://github.com/manaia-2021/KIND"
+									and development.'
+					projectTech='React, MUI'
+					liveLink='https://kind-manaia.herokuapp.com'
+					githubLink='https://github.com/manaia-2021/KIND'
 				/>
 				<ProjectCard
-					projectName="TOP Sign up form"
-					aboutProject="A project using flex-box with a little bit of grid. This project focused on forms and input validation. The other branch of this project includes the Constraint Validation API to validate all the inputs as the user interacts with them."
-					projectTech="HTML, CSS"
-					liveLink="https://chris-wagg.github.io/TOP-sign-up-form/"
-					githubLink="https://github.com/Chris-Wagg/TOP-sign-up-form"
+					projectName='TOP Sign up form'
+					aboutProject='A project using flex-box with a little bit of grid. This project focused on forms and input validation. The other branch of this project includes the Constraint Validation API to validate all the inputs as the user interacts with them.'
+					projectTech='HTML, CSS'
+					liveLink='https://chris-wagg.github.io/TOP-sign-up-form/'
+					githubLink='https://github.com/Chris-Wagg/TOP-sign-up-form'
 				/>
 				<ProjectCard
-					projectName="TOP Admin Dashboard"
-					aboutProject="A challenge from The Odin Project using both flex and grid concepts together to create a flexible and dynamic page with multiple elements"
-					projectTech="HTML, CSS"
-					liveLink="https://chris-wagg.github.io/TOP-Admin-Dashboard/"
-					githubLink="https://github.com/Chris-Wagg/TOP-Admin-Dashboard"
+					projectName='TOP Admin Dashboard'
+					aboutProject='A challenge from The Odin Project using both flex and grid concepts together to create a flexible and dynamic page with multiple elements'
+					projectTech='HTML, CSS'
+					liveLink='https://chris-wagg.github.io/TOP-Admin-Dashboard/'
+					githubLink='https://github.com/Chris-Wagg/TOP-Admin-Dashboard'
 				/>
 				<ProjectCard
-					projectName="Top Landing page"
-					aboutProject="Another Odin Project challenge to put multiple flexbox concepts together to make one webpage."
-					projectTech="HTML, CSS"
-					liveLink="https://chris-wagg.github.io/TOP-landing-page/"
-					githubLink="https://github.com/Chris-Wagg/TOP-landing-page"
+					projectName='Top Landing page'
+					aboutProject='Another Odin Project challenge to put multiple flexbox concepts together to make one webpage.'
+					projectTech='HTML, CSS'
+					liveLink='https://chris-wagg.github.io/TOP-landing-page/'
+					githubLink='https://github.com/Chris-Wagg/TOP-landing-page'
 				/>
 			</div>
 		</section>
